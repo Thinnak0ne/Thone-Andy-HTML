569ff9d47080bc30274b53cff12cc2494467e93e
ksm7529 <ksm7529@gmail.com>
2022-06-05 22:44:16 +0700

    first say name

---
+++ say name/script.js
@@ -0,0 +1,12 @@
+let a = 1 + 2
+console.log(a)
+console.log("Hello World")
+console.log("Hello World")
+console.log("Hello World")
+
+function Naja() {
+    let x = document.getElementById("name").value
+    alert("Hello " + x)
+}
+
+//alert("hello world")
